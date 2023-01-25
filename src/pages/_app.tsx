c8b21d6ae558a5bb56bb8afd892c84940d0a732b
shatfield4 <seanhatfield5@gmail.com>
2023-01-25 13:22:54 -0800

    added to ui to be more dynamic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,9 @@ import { Dna } from 'react-loader-spinner';
 
 function App() {
   const [prompt, setPrompt] = useState("");
+  const [fromEmail, setFromEmail] = useState("");
   const [response, setResponse] = useState("");
+  const [trainingRules, setTrainingRules] = useState(`you are customer support for a brand called PocketProjectr. the brand sells mini projectors that can be hooked up to any device with video output. please respond to the following email in a polite manner and if they are asking where their order is offer them a 20% discount code 20CODEOFF on another one and assure them that their order is on the way. make sure to let the customer know that we have indeed sent their order from our fulfilment center but the shipping company is at fault if they are asking about shipping or not recieving their order. let the customer know that if needed we will send another order to them to make sure they recieve their product after we hear back from Pitney Bowes if they are asking about shipping or not recieving their order. use the excuse that there was an issue with the shipping company and we are working to get an updated tracking number asap from them and we reached out to the shipping company, Pitney Bowes, on their behalf and opened a case with the company if they are asking about shipping or not recieving their order. if they are saying and item is defective or they would like to return, convince them that it may be user error. for the signature sign the email with "Best, John, PocketProjectr Support".`);
   const [isLoading, setIsLoading] = useState(false);
   const [responseType, setResponseType] = useState('');
 
@@ -33,7 +35,10 @@ function App() {
       },
       body: JSON.stringify({ 
         prompt: prompt,
-        responseType: responseType}),
+        fromEmail: fromEmail,
+        responseType: responseType,
+        trainingRules: trainingRules
+      }),
     });
     const text = await response.text();
     setResponse(text);
@@ -44,22 +49,47 @@ function App() {
 
     <div className="App">
       <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg">
+        <h2 className="text-3xl font-bold text-center mb-5">AI Email Customer Support Demo</h2>
+        <p className="text-gray-700 text-center mb-4">Enter a sample email and select a response type to get a response from the AI.</p>
+        <div className='mb-4'>
+          <label className="block text-gray-700 font-medium mb-2" htmlFor="trainingRules">
+            Training Rules
+          </label>
+          <textarea
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500 h-40"
+            id="trainingRules"
+            value={trainingRules}
+            onChange={(e) => setTrainingRules(e.target.value)}
+            placeholder="Enter your training rules here" />
+        </div>
         <div className="mb-4">
-          <label className="block text-gray-700 font-medium mb-2" htmlFor="prompt">
-            Support Bot
+          <label className="block text-gray-700 font-medium mb-2" htmlFor="fromEmail">
+            From Email
           </label>
           <input
             className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500"
-            id="prompt"
+            id="fromEmail"
             type="text"
+            value={fromEmail}
+            onChange={(e) => setFromEmail(e.target.value)}
+            placeholder="Enter the from email here"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-medium mb-2" htmlFor="prompt">
+            Email Body
+          </label>
+          <textarea
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500"
+            id="prompt"
             value={prompt}
             onChange={(e) => setPrompt(e.target.value)}
-            placeholder="Enter your prompt here"
+            placeholder="Enter your email's body here"
           />
         </div>
         <div className="mb-4">
           <label className="block text-gray-700 font-medium mb-2" htmlFor="response-type">
-            Response Type
+            Response Tone
           </label>
           <select 
             className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-indigo-500"
@@ -89,11 +119,8 @@ function App() {
         </div>
       </form>
       <div className="bg-gray-100 p-6 rounded-lg leading-tight">
-        {/* <p className="text-gray-700">{response}</p> */}
         <p className="text-gray-700" dangerouslySetInnerHTML={{__html: response}} />
       </div>
-
-      {/* <div dangerouslySetInnerHTML={{__html: htmlString}} /> */}
       <div className="mt-10">
         {/* <button className="bg-indigo-500 text-white py-2 px-4 rounded-lg hover:bg-indigo-600 mx-5" onClick={handleLogin}>
           Login with Gmail
--- src/pages/api/ai.ts
+++ src/pages/api/ai.ts
@@ -1,7 +1,6 @@
 import OpenAI from "openai-api";
 import { NextApiRequest, NextApiResponse } from 'next';
 
-// require("dotenv").config();
 // instantiate OpenAI API wrapper
 const openai = new OpenAI(process.env.OPENAI_API_KEY!);
 
@@ -10,13 +9,12 @@ const openai = new OpenAI(process.env.OPENAI_API_KEY!);
 const api = async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
     case 'POST':
-      const { prompt, responseType } = req.body;
+      const { prompt, responseType, fromEmail, trainingRules } = req.body;
       const maxTokens = 300;
       const temperature = 0.9;
 
       let responseTypePrompt = `respond in a tone of voice of ${responseType}`;
-      let fullPrompt = `${responseTypePrompt} and you are customer support for a brand called PocketProjectr. the brand sells mini projectors that can be hooked up to any device with video output. please respond to the following email in a polite manner and if they are asking where their order is offer them a 20% discount code 20CODEOFF on another one and assure them that their order is on the way. make sure to let the customer know that we have indeed sent their order from our fulfilment center but the shipping company is at fault if they are asking about shipping or not recieving their order. let the customer know that if needed we will send another order to them to make sure they recieve their product after we hear back from Pitney Bowes if they are asking about shipping or not recieving their order. use the excuse that there was an issue with the shipping company and we are working to get an updated tracking number asap from them and we reached out to the shipping company, Pitney Bowes, on their behalf and opened a case with the company if they are asking about shipping or not recieving their order. if they are saying and item is defective or they would like to return, convince them that it may be user error. write the response as html and do not use /n for line breaks, use <br>. for the signature sign the email with "Best, John, PocketProjectr Support":`;
-      fullPrompt += prompt;
+      let fullPrompt = `${responseTypePrompt} and ${trainingRules} write the response as html and do not use /n for line breaks, use <br>: from:${fromEmail}: do not continue the sentence, this is the full email: ${prompt}`;
 
       const openaiResponse = await openai.complete({
         engine: "text-davinci-003",
